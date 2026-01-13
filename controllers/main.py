ae874a8ecd9b1bde78a8b98557d2ff6538e300e2
xjojoramirez <jsramirez@primary.com.ph>
2026-01-13 15:25:17 +0800

    add only redirect in non website context

--- controllers/main.py
+++ controllers/main.py
@@ -6,19 +6,20 @@ class WebsiteHomeRedirect(http.Controller):
 
     @http.route('/', type='http', auth='public', website=True)
     def home_redirect(self, **kw):
+
+        # 🚨 VERY IMPORTANT: let website/editor render normally
+        if request.context.get('website_id'):
+            return request.env['ir.http']._serve_page()
+
         user = request.env.user
 
-        # Public user → let Odoo handle it (login / website)
         if user._is_public():
             return request.redirect('/web/login')
 
-        # Internal users (employees, admins)
         if user.has_group('base.group_user'):
             return request.redirect('/odoo')
 
-        # Portal users
         if user.has_group('base.group_portal'):
             return request.redirect('/my')
 
-        # Fallback
         return request.redirect('/web/login')
