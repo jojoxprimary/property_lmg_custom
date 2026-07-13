aa6e7393f999794cb24b798efdcd0b321441831e
jojoxprimary <jsramirez@primary.com.ph>
2026-07-13 09:53:48 +0800

    remove accounting in depends so only invoice is active

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Property LMG Rental Substate Custom',
     'version': '1.0',
-    'depends': ['sale_renting', 'sale_subscription', 'sale_management', 'base_substate', 'sale_substate', 'stock', 'crm', 'accountant', 'helpdesk', 'website', 'web_studio'],
+    'depends': ['sale_renting', 'sale_subscription', 'sale_management', 'base_substate', 'sale_substate', 'stock', 'crm', 'helpdesk', 'website', 'web_studio'],
     'author': 'Homebrew',
     'category': 'Sales/Rental',
     'data': [
